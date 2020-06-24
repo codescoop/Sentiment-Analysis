7db74d24c01beb11f9a39cc02a41a8fc85b7322c
Yogesh Yadav <yogesh7132@gmail.com>
2020-06-24 20:11:12 +0530

    Added SentimentAnalysis.py

---
+++ SentimentAnalysis.py
@@ -0,0 +1,31 @@
+import string
+
+text = open("read.txt", encoding="utf-8").read()
+lower_case = text.lower()
+# print(lower_case)          ## Converting to lowercase
+
+# print(string.punctuation)
+cleaned_text = lower_case.translate(str.maketrans("", "", string.punctuation))
+# print(cleaned_text)         ## Removing punctuations
+
+tokenized_words = cleaned_text.split()
+# print(tokenized_words)
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+## removing stop_words
+final_words = []
+for word in tokenized_words:
+    if word not in stop_words:
+        final_words.append(word)
+
+print(final_words)
