0450e971fc71a107a8416c4aee1f95db1cbd0b80
Yogesh Yadav <yogesh7132@gmail.com>
2020-06-29 17:59:38 +0530

    Added NLTK module

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,31 +1,63 @@
 import string
+from collections import Counter
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
 
 text = open("read.txt", encoding="utf-8").read()
 lower_case = text.lower()
-# print(lower_case)          ## Converting to lowercase
+# print(lower_case)
 
 # print(string.punctuation)
 cleaned_text = lower_case.translate(str.maketrans("", "", string.punctuation))
-# print(cleaned_text)         ## Removing punctuations
+# print(cleaned_text)
 
-tokenized_words = cleaned_text.split()
+tokenized_words = word_tokenize(cleaned_text, "english")
 # print(tokenized_words)
 
-stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
-              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
-              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
-              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
-              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
-              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
 ## removing stop_words
 final_words = []
 for word in tokenized_words:
-    if word not in stop_words:
+    if word not in stopwords.words("english"):
         final_words.append(word)
 
-print(final_words)
+# print(final_words)
+
+## Applying NLP Algorithm
+emotion_list = []
+with open("emotions.txt", "r") as emotion_file:
+    for line in emotion_file:
+        clear_line = line.replace('\n', " ").replace("'", "").replace(",", "").strip()
+        word, emotion = clear_line.split(":")
+        # print(word,emotion)
+
+        if word in final_words:
+            emotion_list.append(emotion)
+
+# print(emotion_list)
+
+## counting emotions
+word_count = Counter(emotion_list)
+# print(word_count)
+
+
+def sentiment_analyse(sentiment_text):
+    score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+    # print(score)
+    neg = score["neg"]
+    pos = score["pos"]
+    if neg > pos:
+        print("Negative Sentiment")
+    else:
+        print("Positive Sentiment")
+
+sentiment_analyse(cleaned_text)
+
+fig, axl = plt.subplots()
+axl.bar(word_count.keys(), word_count.values())
+fig.autofmt_xdate()
+# plt.bar(word_count.keys(),word_count.values())
+plt.savefig("graph.png")
+plt.show()
